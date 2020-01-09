0b74e53889dc7e2dc5f02301a34bab0d8b9562e8
Viktor Savonin <viktor.savonin@meawallet.com>
2020-01-09 19:48:13 +0200

    TaxRateProvider Mock

--- src/test/java/lv/javaguru/junit/workshop/lesson_1/TaxCalculatorImplTest.java
+++ src/test/java/lv/javaguru/junit/workshop/lesson_1/TaxCalculatorImplTest.java
@@ -1,25 +1,38 @@
 package lv.javaguru.junit.workshop.lesson_1;
 
+import lv.javaguru.junit.workshop.lesson_2.TaxRate;
 import lv.javaguru.junit.workshop.lesson_2.TaxRateProvider;
 import lv.javaguru.junit.workshop.lesson_2.TaxRateProviderStub;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
 public class TaxCalculatorImplTest {
 
+    private TaxRateProvider taxRateProvider;
     private TaxCalculator calculator;
 
     @Before
     public void setup() {
-        TaxRateProvider taxRateProvider = new TaxRateProviderStub();
+        taxRateProvider = Mockito.mock(TaxRateProvider.class);
         calculator = new TaxCalculatorImpl(taxRateProvider);
     }
 
     @Test
     public void income20000() {
+        TaxRate taxRate = new TaxRate();
+        taxRate.setBorder(20000.0);
+        taxRate.setLowerRate(0.25);
+        taxRate.setUpperRate(0.4);
+
+        Mockito.when(taxRateProvider.getTaxRate(2000))
+                .thenReturn(taxRate);
+
         checkResult(20000.0, 2000, 5000.0);
+
+        Mockito.verify(taxRateProvider).getTaxRate(2000);
     }
 
     @Test
